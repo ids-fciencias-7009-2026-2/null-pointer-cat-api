e4621da315c8f460d8d146b5f3d627a74e50dee4
ReEspinosa <rbkespinosa@ciencias.unam.mx>
2026-02-17 13:11:28 -0600

    Nuevo comentario

---
+++ src/main/kotlin/com/nullpointercats/sys/adopta/AdoptaApplication.kt
@@ -0,0 +1,11 @@
+package com.nullpointercats.sys.adopta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdoptaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdoptaApplication>(*args)
+}
---
+++ src/test/kotlin/com/nullpointercats/sys/adopta/AdoptaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nullpointercats.sys.adopta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdoptaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
