c1b2dbbf07996611ee1b9b11aa66ccc97090147c
aricc24 <aricc24@ciencias.unam.mx>
2026-02-21 16:29:25 -0600

    docs: document LogoutResponse DTO

--- src/main/kotlin/com/nullpointercats/sys/adopta/dto/response/LogoutResponse.kt
+++ src/main/kotlin/com/nullpointercats/sys/adopta/dto/response/LogoutResponse.kt
@@ -1,6 +1,17 @@
 package com.nullpointercats.sys.adopta.dto.response
 
+/**
+ * DTO used to send logout confirmation information back to
+ * the client after a successful operation.
+ */
+
 data class LogoutResponse(
+    /**
+     * Unique identifier of the user who logged out.
+     */
     val userId: String,
+    /**
+     * Date and time when the logout operation occurred.
+     */
     val logoutDateTime: String
 )
