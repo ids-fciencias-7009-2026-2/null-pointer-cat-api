9314507bed1922c64d5e83274602955e51d7282b
WendySC <wendy.c2502@gmail.com>
2026-02-21 19:32:12 -0600

    chore: fix comment syntax of User domain

--- src/main/kotlin/com/nullpointercats/sys/adopta/domain/User.kt
+++ src/main/kotlin/com/nullpointercats/sys/adopta/domain/User.kt
@@ -1,42 +1,42 @@
 package com.nullpointercats.sys.adopta.domain
 
-/*
-* Represents a user within Adopta system.
-* */
+/**
+ * Represents a user within Adopta system
+ */
 data class User(
 
-    /*
+    /**
     * Unique identifier of the user.
     * */
     val id        : String,
 
-    /*
+    /**
     * Public display name of the user.
     * */
     var username  : String,
 
-    /*
+    /**
     * Email address of the user.
     *
     * */
     var email     : String,
 
-    /*
+    /**
     * Password of the user. Must be encrypted.
     * */
     var password  : String,
 
-    /*
+    /**
     * Given name of the user.
     * */
     var firstName : String,
 
-    /*
+    /**
     * Family name of the user.
     * */
     var lastName  : String,
 
-    /*
+    /**
     * Postal code representing the user's location.
     * */
     var zipCode   : Int
