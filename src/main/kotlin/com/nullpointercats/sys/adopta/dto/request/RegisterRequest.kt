145176e4504bc6f149c2832beedd4ebc70cd7a0b
Ximena  Tapia <jaimari@Ximenas-MacBook.local>
2026-02-23 16:07:28 -0600

    refactor: removed  RegisterRequest, RegisterResponse DTO for user.

--- src/main/kotlin/com/nullpointercats/sys/adopta/dto/request/RegisterRequest.kt
+++
@@ -1,8 +0,0 @@
-data class RegisterRequest(
-    val username: String,
-    val email: String,
-    val password: String,
-    val firstName: String,
-    val lastName: String,
-    val zipCode: Int
-)
--- src/main/kotlin/com/nullpointercats/sys/adopta/dto/response/RegisterResponse.kt
+++
@@ -1,9 +0,0 @@
-data class RegisterResponse(
-    val id: String,
-    val username: String,
-    val email: String,
-    val password: String,
-    val firstName: String,
-    val lastName: String,
-    val zipCode: Int
-)
