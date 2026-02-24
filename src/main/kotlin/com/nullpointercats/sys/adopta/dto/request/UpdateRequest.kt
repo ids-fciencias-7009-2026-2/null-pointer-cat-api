2f46d8d2f91cb909791d4bb1decc86373ff1436c
ReEspinosa <rbkespinosa@ciencias.unam.mx>
2026-02-23 23:01:25 -0600

    feat: add PUT /users endpoint for updating user information

---
+++ src/main/kotlin/com/nullpointercats/sys/adopta/dto/request/UpdateRequest.kt
@@ -0,0 +1,28 @@
+package com.nullpointercats.sys.adopta.dto.request
+
+/**
+ * DTO used to receive user update data
+ * within the Adopta system.
+ */
+data class UpdateRequest(
+
+    /**
+     * New public display name of the user.
+     */
+    val username: String,
+
+    /**
+     * New given name of the user.
+     */
+    val firstName: String,
+
+    /**
+     * New family name of the user.
+     */
+    val lastName: String,
+
+    /**
+     * New postal code representing the user's location.
+     */
+    val zipCode: Int
+)
