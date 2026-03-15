3f3c67ce4e58ae80ad3e9fe6ea1953d9f90be2d0
aricc24 <aricc24@ciencias.unam.mx>
2026-03-14 22:19:48 -0600

    docs: add findByEmail docs

--- src/main/kotlin/com/nullpointercats/sys/adopta/repositories/UserRepository.kt
+++ src/main/kotlin/com/nullpointercats/sys/adopta/repositories/UserRepository.kt
@@ -9,6 +9,20 @@ import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : CrudRepository<UserEntity, Int> {
 
+
+ /**
+     * Retrieves a user from the database using their email address.
+     *
+     * Spring Data JPA automatically derives the SQL query from
+     * the method name. Internally, this corresponds to a query
+     * similar to:
+     *
+     * SELECT * FROM adoption_user WHERE email = ?
+     *
+     * @param email The email address used to search for the user.
+     * @return The UserEntity if a user with the given email exists,
+     *         or null if no matching user is found.
+     */
 fun findByEmail(email: String): UserEntity?
 
 }
