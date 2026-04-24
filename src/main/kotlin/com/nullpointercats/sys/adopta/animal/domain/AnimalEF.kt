9cda7faa113288a28be4e28c89e90e1187d83001
WendySC <wendy.c2502@gmail.com>
2026-04-24 00:05:24 -0600

    feat: AnimalRegisterRequest to Animal domain

--- src/main/kotlin/com/nullpointercats/sys/adopta/animal/domain/AnimalEF.kt
+++ src/main/kotlin/com/nullpointercats/sys/adopta/animal/domain/AnimalEF.kt
@@ -1,7 +1,26 @@
 package com.nullpointercats.sys.adopta.animal.domain
-
+import com.nullpointercats.sys.adopta.animal.dto.request.AnimalRegisterRequest
 import com.nullpointercats.sys.adopta.animal.entities.AnimalEntity
-import com.nullpointercats.sys.adopta.user.domain.toUser
+import com.nullpointercats.sys.adopta.user.domain.*
+
+fun AnimalRegisterRequest.toDomain(
+    publisherDomain: User,
+    breedDomain: Breed?
+): Animal {
+    return Animal(
+        idAnimal = null,
+        animalName = this.animalName,
+        species = this.species,
+        dateOfBirth = this.dateOfBirth,
+        description = this.description,
+        size = this.size,
+        animalZipcode = this.animalZipcode,
+
+        publisher = publisherDomain,
+        breed = breedDomain,
+        photos = this.photosURLs.map { url -> Photo(url = url) }
+    )
+}
 
 fun AnimalEntity.toDomain(): Animal {
     return Animal(
