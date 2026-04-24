0cb9ac2fe60d7a3b08af54c622a7aa3b54ee4419
WendySC <wendy.c2502@gmail.com>
2026-04-23 22:04:43 -0600

    docs : Breed domain

--- src/main/kotlin/com/nullpointercats/sys/adopta/animal/domain/Breed.kt
+++ src/main/kotlin/com/nullpointercats/sys/adopta/animal/domain/Breed.kt
@@ -4,14 +4,28 @@ package com.nullpointercats.sys.adopta.animal.domain
  * Represents a breed catalogue entry for dogs or cats in the Adopta system.
  */
 data class Breed (
+    /**
+     * Unique identifier of the breed.
+     * */
+    val idBreed: Int ? = null,
 
-    val idBreed: Int,
-
+    /**
+     * Name of the breed.
+     * */
     val breedName : String,
 
+    /**
+     * County or regin of origin of the breed.
+     * */
     val origin : String ? = null,
 
+    /**
+     * Brief description of the behavior of the breed.
+     * */
     val temperament: String ? = null,
 
+    /**
+     * Average life span of the breed.
+     * */
     val lifeSpan : String ? = null,
 )
