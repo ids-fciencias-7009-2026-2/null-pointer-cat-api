2be5c4a8ce84f1da5e9bfed6288b21cd1ae29e4c
WendySC <wendy.c2502@gmail.com>
2026-04-23 22:04:08 -0600

    docs: Photo domain

--- src/main/kotlin/com/nullpointercats/sys/adopta/animal/domain/Photo.kt
+++ src/main/kotlin/com/nullpointercats/sys/adopta/animal/domain/Photo.kt
@@ -3,13 +3,25 @@ package com.nullpointercats.sys.adopta.animal.domain
 /**
  * Represents a photograph associated with an animal.
  */
+
 data class Photo (
+    /**
+     * Unique identifier of the photo.
+     * */
+    var idPhoto: Int? = null,
 
-    var idPhoto: Int,
-
+    /**
+     * URL of the photo.
+     * */
     var url: String,
 
+    /**
+     * Image width in pixels.
+     * */
     var width: Int ? = null,
 
+    /**
+     * Image height in pixels.
+     * */
     var height: Int ? = null,
 )
